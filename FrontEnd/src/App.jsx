c238fcd25356ae575d1461b3e78b4b76e238b691
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-05 08:56:29 -0500

    Create base home page and login page for test routers

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
 const App = () => {
-  return <div className="container text-center text-xl font-bold text-red-500">App</div>;
+  return (
+    <>
+      <div className="container text-center text-xl font-bold text-red-500">
+        App
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />}  />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
