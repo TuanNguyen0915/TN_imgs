17a1be68ca9a12cf2afef5eca25118143c65dd90
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-05 08:58:37 -0500

    add basic middleware

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,18 @@
 import 'dotenv/config'
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 const app = express()
 const PORT = process.env.PORT
 
 // connect mongoDB
 import './configs/database.js'
+
+// basic middleware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.get('/', (req,res) => {
   res.send('BackEnd')
