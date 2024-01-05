1a9b780a3e4c80f2c1360ae27de70647fa682d0c
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-05 10:55:42 -0500

    using firebase for googleOAuth

--- FrontEnd/src/components/GoogleLogin.jsx
+++ FrontEnd/src/components/GoogleLogin.jsx
@@ -1,9 +1,30 @@
 import googleIcon from '../assets/google-icon.png'
+import { app } from '../utils/firebase'
+import {GoogleAuthProvider, getAuth, signInWithPopup}  from 'firebase/auth'
 
 const GoogleLogin = () => {
+  const handleOAuth = async () => {
+    try {
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth(app)
+      const result = await signInWithPopup(auth,provider)
+      const formData ={
+        name: result.user.displayName,
+        email: result.user.email,
+        avatar: result.user.photoURL
+      }
+      console.log(formData)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
   return (
     <div>
-      <button className='bg-slate-300 px-4 py-2 rounded-lg shadow-2xl flex items-center justify-between h-[50px] gap-2'>
+      <button 
+      className='bg-slate-300 px-4 py-2 rounded-lg shadow-2xl flex items-center justify-between h-[50px] gap-2'
+      onClick={handleOAuth}
+      >
         <img src={googleIcon} alt="google icon" className='object-contain h-full flex items-center justify-center'/>
         <p className='w-full'>Sign in with Google</p>
       </button>  
