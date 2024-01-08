bc9c3b1480b67d690cfc17290389fcfdc4afa987
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-08 09:50:06 -0500

    add function add saved userId for photoService  FE

--- FrontEnd/src/services/image.js
+++ FrontEnd/src/services/image.js
@@ -1,13 +1,32 @@
-const SERVER_URL = import.meta.env.VITE_SERVER_URL
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 const allImages = async () => {
   try {
-    const res = await fetch(`${SERVER_URL}/image/all`)
-    const json = await res.json()
-    return json.data
+    const res = await fetch(`${SERVER_URL}/image/all`);
+    const json = await res.json();
+    return json.data;
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 
-export {allImages}
+const imageSaved = async (userId, imageId) => {
+  const body = {
+    userId,
+    imageId
+  }
+  try {
+    const res = await fetch(`${SERVER_URL}/image/saved`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+    const json = await res.json();
+    return json.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+export { allImages, imageSaved };
