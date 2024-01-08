c042f4f9e5d3d990a88ca935488c487c0d2a41d5
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-08 10:29:49 -0500

    add populate to show addBy info

--- BackEnd/controllers/photo.js
+++ BackEnd/controllers/photo.js
@@ -16,7 +16,7 @@ const uploadImage = async (req, res, next) => {
 
 const allImages = async (req, res, next) => {
   try {
-    let photos = await Photo.find({})
+    let photos = await Photo.find({}).populate('addBy')
     res.status(200).json({ success: true, message: 'All images', data: photos })
   } catch (error) {
     res.status(500)
