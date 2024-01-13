dd0e88b2871bfaf4d6620d64fe9408ea26a5dff5
Tuan Nguyen <tuan.nat915@gmail.com>
2024-01-13 10:51:57 -0500

    updated be

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -11,7 +11,7 @@ import { router as authRouter } from './routes/auth.js'
 import { router as imgRouter } from './routes/image.js'
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const corsOption = {
   origin: true,
   credentials: true
